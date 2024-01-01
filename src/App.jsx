ee0f42ad6fe2bb0b57f5f3ffcbf1cb5efd3bbff2
100ngram <sangramjitdutta@gmail.com>
2024-01-01 22:29:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useNavigate } from "react-router-dom";
+
+function App() {
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
+  const [inpVal, setInpVal] = useState("");
+  const [inpErr, setInpErr] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Array of promises representing multiple API calls
+        const promises = [
+          axios.get("https://jsonplaceholder.typicode.com/posts"),
+          axios.get("https://jsonplaceholder.typicode.com/albums"),
+        ];
+
+        // Use Promise.all to wait for all promises to resolve
+        const [response1, response2] = await Promise.all(promises);
+        console.log("promises", promises);
+        setIsFetched(true);
+        // Update state with data
+        setData1(response1.data);
+        setData2(response2.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const numberRegex = /^[1-9][0-9]?$|^100$/;
+          if (numberRegex.test(inpVal)) {
+            navigate(`/${inpVal}`);
+          } else {
+            setInpErr("error");
+          }
+        }}
+      >
+        <input
+          type="text"
+          value={inpVal}
+          placeholder="Enter a Id "
+          onChange={(e) => {
+            setInpVal(e.target.value);
+            setInpErr("");
+          }}
+        />
+        <button type="submit">goto link</button>
+        {inpErr && (
+          <div style={{ color: "red" }}> Enter a Valid ID from 1-100 !</div>
+        )}
+      </form>
+      {isFetched ? (
+        <div>
+          <h1>Data from API 1</h1>
+          <pre>{JSON.stringify(data1, null, 2)}</pre>
+
+          <h1>Data from API 2</h1>
+          <pre>{JSON.stringify(data2, null, 2)}</pre>
+        </div>
+      ) : (
+        <ClipLoader color="green" size={200} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/profile.jsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+
+export default function ProfilePage() {
+  // Get the userId param from the URL.
+  let { Id } = useParams();
+  const [data1, setData1] = useState([]);
+  const [data2, setData2] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Array of promises representing multiple API calls
+        const promises = [
+          axios.get(`https://jsonplaceholder.typicode.com/posts/${Id}`),
+          axios.get(`https://jsonplaceholder.typicode.com/albums/${Id}`),
+        ];
+
+        // Use Promise.all to wait for all promises to resolve
+        const [response1, response2] = await Promise.all(promises);
+        console.log("promises", promises);
+        setIsFetched(true);
+        // Update state with data
+        setData1(response1.data);
+        setData2(response2.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>ID : {Id}</h1>
+      {isFetched ? (
+        <div>
+          <h2>Data from API 1</h2>
+          <pre>{JSON.stringify(data1, null, 2)}</pre>
+
+          <h2>Data from API 2</h2>
+          <pre>{JSON.stringify(data2, null, 2)}</pre>
+        </div>
+      ) : (
+        <ClipLoader color="green" size={200} />
+      )}
+    </div>
+  );
+}
