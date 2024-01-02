cb1093af0e56cef92f58da7f88b7b46b0f0e3a6e
100ngram <sangramjitdutta@gmail.com>
2024-01-02 13:55:55 +0530

    added strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import HomePage from "./components/homePage.jsx";
 import { routes } from "./routes/routes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <RouterProvider router={routes} />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={routes} />
+  </React.StrictMode>
 );
